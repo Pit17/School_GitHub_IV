b2a5fd71ac25d427fe4c5bba892dd9f8d35d63d5
Pit17 <malzonepietro3@gmail.com>
2024-09-19 11:26:04 +0200

    Esercizi_Informatica_Date

--- Informatica Molara/Informatica-Molara/ConsoleAppEsericiziDate/ConsoleAppEsericiziDate/Program.cs
+++ Informatica Molara/Informatica-Molara/ConsoleAppEsericiziDate/ConsoleAppEsericiziDate/Program.cs
@@ -4,6 +4,41 @@
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("+-------------------------------------------------------------+");
+                Console.WriteLine("|                                                             |");
+                Console.WriteLine("|  Benvenuto!                                                 |");
+                Console.WriteLine("|  Inserisci il numero dell'esercizio che vuoi eseguire       |");
+                Console.WriteLine("|  1) Calcola la differenza tra due date                      |");
+                Console.WriteLine("|  2) Verifica se un'ora è compresa tra due orari             |");
+                Console.WriteLine("|  3) Trova la coppia di date con la minima differenza        |");
+                Console.WriteLine("|  4) Esci                                                    |");
+                Console.WriteLine("|                                                             |");
+                Console.WriteLine("+-------------------------------------------------------------+");
+                int scelta = int.Parse(Console.ReadLine());
+                switch (scelta)
+                {
+                    case 1:
+                        Ex1(args);
+                        break;
+                    case 2:
+                        Ex2(args);
+                        break;
+                    case 3:
+                        Ex3(args);
+                        break;
+                    case 4:
+                        return;
+                    default:
+                        Console.WriteLine("Scelta non valida");
+                        break;
+                }
+            }
+            
+        }
+        static void Ex1(string[] args)
+        {
             DateOnly data1;
             DateOnly data2;
             while (true)
@@ -41,6 +76,43 @@
             if (Start <= ora && ora <= End) Console.WriteLine("INTERNO");
             else Console.WriteLine("ESTERNO");
         }
+        static void Ex3(string[] args){
+            DateTime[] dates = {
+            new DateTime(2014, 6, 14, 6, 32, 0),
+            new DateTime(2014, 7, 10, 23, 49, 0),
+            new DateTime(2015, 1, 10, 1, 16, 0),
+            new DateTime(2014, 12, 20, 21, 45, 0),
+            new DateTime(2014, 6, 2, 15, 14, 0)
+        };
 
+        
+        DateTime min1 = dates[0];
+        DateTime min2 = dates[1];
+        double differenzaMin = Math.Abs((dates[0] - dates[1]).TotalSeconds);
+
+        
+        for (int i = 0; i < dates.Length; i++)
+        {
+            for (int j = i + 1; j < dates.Length; j++)
+            {
+                
+                double differenza = Math.Abs((dates[i] - dates[j]).TotalSeconds);
+
+                
+                if (differenza < differenzaMin)
+                {
+                    differenzaMin = differenza;
+                    min1 = dates[i];
+                    min2 = dates[j];
+                }
+            }
+        }
+
+        
+        Console.WriteLine("La coppia di date con la minima distanza è:");
+        Console.WriteLine($"{min1} e {min2}");
+        Console.WriteLine($"Differenza: {TimeSpan.FromSeconds(differenzaMin)} giorni");
+        }
     }
+    
 }
