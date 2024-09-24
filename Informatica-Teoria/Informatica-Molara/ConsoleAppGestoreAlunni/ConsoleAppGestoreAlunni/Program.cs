de94be16041e433aefde1dcf1d9c15ee8fd4df98
Pietro Malzone <pietro.malzone.stud@ispascalcomandini.it>
2024-09-24 11:45:23 +0200

    Migliorie

---
+++ Informatica-Teoria/Informatica-Molara/ConsoleAppGestoreAlunni/ConsoleAppGestoreAlunni/Program.cs
@@ -0,0 +1,88 @@
+﻿//Pietro Malzone 4H Console App per gestire dati degli studenti 24/09/2024
+using System.ComponentModel;
+
+namespace ConsoleAppGestoreAlunni
+{
+    class Alunno//classe che gestisce i dati di ogni studente
+    {
+        private string Nome;
+        private string Cognome;
+        private char Sesso;
+        private DateOnly data_nascita;
+        private string Classe;
+        private int Anno;
+        private char Sezione;
+        private string indirizzo_scolastico;
+
+        public Alunno(string nome, string cognome, char sesso, DateOnly data, string classe, string indirizzo)//costruttore
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Sesso = sesso;
+            data_nascita = data;
+            Classe = classe;
+            Anno = int.Parse(Classe[0].ToString());
+            Sezione= Classe[1];
+            indirizzo_scolastico = indirizzo;
+        }
+
+        public override string ToString()//per stampare i dati
+        {
+            return $"{Nome}\t{Cognome}\t{Sesso}\t{data_nascita}\tANNO:{Anno}\tSEZIONE:{Sezione}\t{indirizzo_scolastico}";
+        }
+
+    }
+
+    internal class Program
+    {
+
+        static void CreazioneGestore(string nome_file)//crea il gestore per inserire i dati in una lista di alunni
+        {
+            List<Alunno> gestore = new();//lista di alunni
+            using (StreamReader sr = new StreamReader(nome_file))
+            {
+                while (!sr.EndOfStream) // la proprietà .EndOfStream è bool e diventa
+                                        // true quando il file finisce
+                {
+                    string[] linea = sr.ReadLine().Split('\t'); // legge la prossima riga del file rimuovendo i tab
+                    if (linea.Length != 6)
+                    {
+
+                        Console.WriteLine("Inserimento alunno fallito!");// in caso non siano presenti tutti i dati di un alunno
+
+                    }
+                    else
+                    {
+                        gestore.Add(new Alunno(linea[0], linea[1], linea[2][0], DateOnly.Parse(linea[3]), linea[4], linea[5]));//altrimenti creo l'alunno
+                    }
+
+                }
+                sr.Close();
+            }
+
+            using (StreamWriter sw = new StreamWriter(@"../../../elenco-alunni-classi-elaborato.txt"))
+            {
+                foreach (Alunno alunno in gestore)//ristampo come da file tutti i dati
+                {
+                    sw.WriteLine(alunno.ToString());
+                    
+                }
+                sw.Close();
+                Console.WriteLine("Elenco riscritto con successo e chiamato 'elenco-alunni-classi-elaborato.txt'");
+            }
+
+        }
+
+        static void Main()//richiamo la funzione
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Pietro Malzone 4H Console App per gestire dati degli studenti 20/09/2024");
+            Console.WriteLine();
+            Console.ForegroundColor= ConsoleColor.Green;
+            CreazioneGestore(@"../../../elenco-alunni-classi.txt");
+            Console.ForegroundColor=ConsoleColor.White;
+            
+        }
+    }
+
+}
