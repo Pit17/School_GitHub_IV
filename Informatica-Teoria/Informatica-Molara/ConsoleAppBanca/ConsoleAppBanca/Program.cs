aab4f7a0d302a35ecddf99a342f54968d218d9d1
Pit17 <malzonepietro3@gmail.com>
2024-10-29 17:04:44 +0100

    banca

--- Informatica-Teoria/Informatica-Molara/ConsoleAppBanca/ConsoleAppBanca/Program.cs
+++ Informatica-Teoria/Informatica-Molara/ConsoleAppBanca/ConsoleAppBanca/Program.cs
@@ -1,83 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace ConsoleAppBanca
+﻿namespace ConsoleAppBanca
 {
     internal class Program
     {
         class Banca
         {
             private List<Cliente> clienti = new List<Cliente>();
-
-            public class Cliente
-            {
-                public string nome;
-                public string cognome;
-                public string codice_fiscale;
-                public double stipendio;
-                public List<PrestitoSemplice> prestiti = new List<PrestitoSemplice>();
-
-                public Cliente(string nome, string cognome, string codice_fiscale, double stipendio)
-                {
-                    this.nome = nome;
-                    this.cognome = cognome;
-                    this.codice_fiscale = codice_fiscale;
-                    this.stipendio = stipendio;
-                }
-
-                public void StampaCliente()
-                {
-                    Console.WriteLine($"Nome : {nome}\nCognome : {cognome}\nCodice-Fiscale : {codice_fiscale}\nStipendio : {stipendio} euro");
-                }
-            }
-
-            public class PrestitoSemplice
-            {
-                protected double capitale;
-                protected double interesse;
-                protected DateOnly data_i;
-                protected DateOnly data_f;
-
-                public PrestitoSemplice(double capitale, double interesse, DateOnly data_i, DateOnly data_f)
-                {
-                    this.capitale = capitale;
-                    this.interesse = interesse;
-                    this.data_i = data_i;
-                    this.data_f = data_f;
-                }
-
-                public double Rata()
-                {
-                    return Math.Ceiling((Montante() / Durata()) / 12 * 100) / 100; 
-                }
-
-                public virtual double Montante()
-                {
-                    return Math.Ceiling((capitale + (capitale * (Durata() * interesse))) * 100) / 100; 
-                }
-
-                public double Durata()
-                {
-                    return Math.Ceiling(((data_f.DayNumber - data_i.DayNumber) / 365.0) * 100) / 100; 
-                }
-
-                public void StampaPrestito()
-                {
-                    Console.WriteLine($"Durata: {Durata()} anni\nRata mensile: {Rata()} euro");
-                }
-            }
-
-            public class PrestitoComposto : PrestitoSemplice
-            {
-                public PrestitoComposto(double capitale, double interesse, DateOnly data_i, DateOnly data_f)
-                    : base(capitale, interesse, data_i, data_f) { }
-
-                public override double Montante()
-                {
-                    int anni = (int)Durata();
-                    return Math.Ceiling((capitale * Math.Pow(1 + interesse, anni)) * 100) / 100; 
-                }
-            }
 
             public void AddCliente(Cliente cliente)
             {
@@ -88,7 +15,7 @@ namespace ConsoleAppBanca
             {
                 for (int i = 0; i < clienti.Count; i++)
                 {
-                    if (clienti[i].codice_fiscale == codice_fiscale)
+                    if (clienti[i].CodiceFiscale == codice_fiscale)
                     {
                         clienti.RemoveAt(i);
                         break;
@@ -100,7 +27,7 @@ namespace ConsoleAppBanca
             {
                 foreach (Cliente cliente in clienti)
                 {
-                    if (cliente.codice_fiscale == codice_fiscale)
+                    if (cliente.CodiceFiscale == codice_fiscale)
                     {
                         return cliente;
                     }
@@ -113,7 +40,7 @@ namespace ConsoleAppBanca
                 Cliente cliente = SearchCliente(codice_fiscale);
                 if (cliente != null)
                 {
-                    cliente.prestiti.Add(prestito);
+                    cliente.AddPrestito(prestito);
                 }
             }
 
@@ -122,7 +49,7 @@ namespace ConsoleAppBanca
                 Cliente cliente = SearchCliente(codice_fiscale);
                 if (cliente != null)
                 {
-                    return cliente.prestiti;
+                    return cliente.GetPrestiti();
                 }
                 return new List<PrestitoSemplice>();
             }
@@ -130,15 +57,101 @@ namespace ConsoleAppBanca
             public double TotalePrestiti(string codice_fiscale)
             {
                 Cliente cliente = SearchCliente(codice_fiscale);
-                double totale = 0;
                 if (cliente != null)
                 {
-                    foreach (PrestitoSemplice prestito in cliente.prestiti)
-                    {
-                        totale += prestito.Montante();
-                    }
+                    return cliente.TotalePrestiti();
                 }
-                return Math.Ceiling(totale * 100) / 100; 
+                return 0;
+            }
+        }
+
+        class PrestitoSemplice
+        {
+            protected double capitale;
+            protected double interesse;
+            protected DateOnly data_i;
+            protected DateOnly data_f;
+
+            public PrestitoSemplice(double capitale, double interesse, DateOnly data_i, DateOnly data_f)
+            {
+                this.capitale = capitale;
+                this.interesse = interesse;
+                this.data_i = data_i;
+                this.data_f = data_f;
+            }
+
+            protected double Rata()
+            {
+                return Math.Ceiling((Montante() / Durata()) / 12 * 100) / 100;
+            }
+
+            public virtual double Montante()
+            {
+                return Math.Ceiling((capitale + (capitale * (Durata() * interesse))) * 100) / 100;
+            }
+
+            protected double Durata()
+            {
+                return Math.Ceiling(((data_f.DayNumber - data_i.DayNumber) / 365.0) * 100) / 100;
+            }
+
+            public void StampaPrestito()
+            {
+                Console.WriteLine($"Durata: {Durata()} anni\nRata mensile: {Rata()} euro");
+            }
+        }
+
+        class PrestitoComposto : PrestitoSemplice
+        {
+            public PrestitoComposto(double capitale, double interesse, DateOnly data_i, DateOnly data_f)
+                : base(capitale, interesse, data_i, data_f) { }
+
+            public override double Montante()
+            {
+                int anni = (int)Durata();
+                return Math.Ceiling((capitale * Math.Pow(1 + interesse, anni)) * 100) / 100;
+            }
+        }
+
+        class Cliente
+        {
+            private string nome;
+            private string cognome;
+            public string CodiceFiscale { get; private set; }
+            private double stipendio;
+            private List<PrestitoSemplice> prestiti = new List<PrestitoSemplice>();
+
+            public Cliente(string nome, string cognome, string codice_fiscale, double stipendio)
+            {
+                this.nome = nome;
+                this.cognome = cognome;
+                this.CodiceFiscale = codice_fiscale;
+                this.stipendio = stipendio;
+            }
+
+            public void AddPrestito(PrestitoSemplice prestito)
+            {
+                prestiti.Add(prestito);
+            }
+
+            public List<PrestitoSemplice> GetPrestiti()
+            {
+                return prestiti;
+            }
+
+            public double TotalePrestiti()
+            {
+                double totale = 0;
+                foreach (PrestitoSemplice prestito in prestiti)
+                {
+                    totale += prestito.Montante();
+                }
+                return Math.Ceiling(totale * 100) / 100;
+            }
+
+            public void StampaCliente()
+            {
+                Console.WriteLine($"Nome : {nome}\nCognome : {cognome}\nCodice-Fiscale : {CodiceFiscale}\nStipendio : {stipendio} euro");
             }
         }
 
@@ -146,27 +159,27 @@ namespace ConsoleAppBanca
         {
             Banca banca = new Banca();
 
-            Banca.Cliente cliente1 = new Banca.Cliente("Mario", "Rossi", "MRORSS80A01H501Z", 2500.0);
-            Banca.Cliente cliente2 = new Banca.Cliente("Anna", "Bianchi", "BNCHAN95P41H501Q", 3000.0);
+            Cliente cliente1 = new Cliente("Mario", "Rossi", "MRORSS80A01H501Z", 2500.0);
+            Cliente cliente2 = new Cliente("Anna", "Bianchi", "BNCHAN95P41H501Q", 3000.0);
+            Cliente cliente3 = new Cliente("Marco", "Balducci", "BLDMRC07B04H294J", 2500.0);
 
-            
             banca.AddCliente(cliente1);
             banca.AddCliente(cliente2);
+            banca.AddCliente(cliente3);
 
-            
             Console.WriteLine("Clienti registrati in banca:");
             cliente1.StampaCliente();
             cliente2.StampaCliente();
+            cliente3.StampaCliente();
 
-            
-            Banca.PrestitoSemplice prestito1 = new Banca.PrestitoSemplice(10000.0, 0.05, new DateOnly(2024, 1, 1), new DateOnly(2026, 1, 1));
-            Banca.PrestitoSemplice prestito2 = new Banca.PrestitoComposto(15000.0, 0.04, new DateOnly(2023, 1, 1), new DateOnly(2026, 1, 1));
+            PrestitoSemplice prestito1 = new PrestitoSemplice(10000.0, 0.05, new DateOnly(2024, 1, 1), new DateOnly(2026, 1, 1));
+            PrestitoSemplice prestito2 = new PrestitoComposto(15000.0, 0.04, new DateOnly(2023, 1, 1), new DateOnly(2026, 1, 1));
+            PrestitoSemplice prestito3 = new PrestitoComposto(200000.0, 0.048, new DateOnly(2024, 10, 29), new DateOnly(2034, 10, 29));
 
-            
             banca.AddPrestito("MRORSS80A01H501Z", prestito1);
             banca.AddPrestito("BNCHAN95P41H501Q", prestito2);
+            banca.AddPrestito("BLDMRC07B04H294J", prestito3);
 
-            
             Console.WriteLine("\nDettagli prestiti di Mario Rossi:");
             foreach (var prestito in banca.SearchPrestiti("MRORSS80A01H501Z"))
             {
@@ -179,17 +192,23 @@ namespace ConsoleAppBanca
                 prestito.StampaPrestito();
             }
 
-            
+            Console.WriteLine("\nDettagli prestiti di Marco Balducci:");
+            foreach (var prestito in banca.SearchPrestiti("BLDMRC07B04H294J"))
+            {
+                prestito.StampaPrestito();
+            }
+
+
             Console.WriteLine($"\nTotale prestiti di Mario Rossi: {banca.TotalePrestiti("MRORSS80A01H501Z")} euro");
             Console.WriteLine($"Totale prestiti di Anna Bianchi: {banca.TotalePrestiti("BNCHAN95P41H501Q")} euro");
+            Console.WriteLine($"Totale prestiti di Marco Balducci: {banca.TotalePrestiti("BLDMRC07B04H294J")} euro");
 
-            
             Console.WriteLine("\nRimozione del cliente Mario Rossi dalla banca.");
             banca.RemoveCliente("MRORSS80A01H501Z");
 
-            
             var clienteRimosso = banca.SearchCliente("MRORSS80A01H501Z");
-            Console.WriteLine(clienteRimosso == null ? "Mario Rossi è stato rimosso correttamente." : "Errore nella rimozione di Mario Rossi.");
+            if (clienteRimosso == null) Console.WriteLine("Mario Rossi è stato rimosso correttamente.");
+            else Console.WriteLine("Errore nella rimozione di Mario Rossi.");
         }
     }
 }
