5d171bed9c34dc31494c8f06e90988556d04f0f9
Pietro Malzone <pietro.malzone.stud@ispascalcomandini.it>
2025-02-22 10:21:23 +0100

    Sitemi client e server c#

---
+++ ConsoleAppClient/ConsoleAppClient/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace ConsoleAppClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StartClient();
+        }
+
+        public static void StartClient()
+        {
+            byte[] bytes = new byte[1024];// Data buffer per i dati 
+
+            try
+            {
+                IPHostEntry host = Dns.GetHostEntry("localhost");// IP preso dal file di configurazione
+                IPAddress ipAddress = host.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);// Creazione del socket
+
+                Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);// Creazione del socket
+
+                try
+                {
+                    sender.Connect(remoteEP);// Connessione al socket
+                    Console.WriteLine("Soclet connected to {0}", sender.RemoteEndPoint.ToString());// Stampa a video del messaggio
+                    byte[] msg = Encoding.ASCII.GetBytes("Ventura + Jessica = <3 <EOF>");// Messaggio da inviare
+                    int bytesSent = sender.Send(msg);// Invio del messaggio
+                    int bytesRec = sender.Receive(bytes);// Ricezione del messaggio
+                    Console.WriteLine("Echoed test = {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));// Stampa a video del messaggio
+                    sender.Shutdown(SocketShutdown.Both);// Chiusura del socket
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)// Gestione delle eccezioni
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+            }catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("\n Press any key...");// Messaggio a video
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleAppServer/ConsoleAppServer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace ConsoleAppServer
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            StartServer();
+
+        }
+
+        public static void StartServer()
+        {
+            IPHostEntry host = Dns.GetHostEntry("localhost");//ip
+            IPAddress iPAddress = host.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(iPAddress, 11000);//porta
+
+            try
+            {
+                Socket listener = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);//socket per la connessione protocollo tcp, tipo stream, ipaddress family
+                listener.Bind(localEndPoint);//associa il socket all'endpoint
+                listener.Listen(10);//ascolta
+                Console.WriteLine("Waiting....");//attesa
+                Socket handler = listener.Accept();//accetta la connessione
+                string data = null;
+                byte[] bytes = null;
+                while (true)//ciclo per ricevere il messaggio
+                {
+                    bytes = new byte[1024];//messaggio
+                    int bytesRec = handler.Receive(bytes);//lunghezza del messaggio
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);//decodifica
+                    if (data.IndexOf("<EOF>") > -1)//se il messaggio contiene <EOF> esce
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("Text received : {0}", data);//stampa il messaggio
+
+                byte[] msg = Encoding.ASCII.GetBytes(data);//codifica il messaggio
+                handler.Send(msg);//invia il messaggio
+                handler.Shutdown(SocketShutdown.Both);//chiude la connessione
+                handler.Close();//chiude il socket
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());//stampa l'errore
+
+            }
+            Console.WriteLine("\n Press any key to continue...");//attesa
+            Console.ReadKey();
+        }
+    
+    }
+
+}
